ffda11f7978c43786b7e5278616f9470aa85a3eb
John Clark <clark@itsmechlark.com>
2021-02-18 11:04:44 +0800

    Change IP allocation to Static

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_public_ip" "pip" {
   name                    = "${random_pet.rand.id}-pip"
   location                = azurerm_resource_group.rg.location
   resource_group_name     = azurerm_resource_group.rg.name
-  allocation_method       = "Dynamic"
+  allocation_method       = "Static"
   idle_timeout_in_minutes = 30
 }
 
